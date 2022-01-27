80e7c61d190a5a686565f578e57a66399dd6d0a3
Michael Albright <msalbrig@gmail.com>
2022-01-27 14:53:29 -0500

    Added exceptions for guessTheNumber

--- guessTheNumber/src/GuessTheNumber.java
+++ guessTheNumber/src/GuessTheNumber.java
@@ -3,20 +3,29 @@ import java.util.Scanner;
 
 public class GuessTheNumber {
     public static void main(String[] args) {
-        int count = 0;
+        int magicNumber;
         int guess;
-        boolean yes;
-        Scanner scan = new Scanner(System.in);
+        String playAgain;
+        String name = "";
+        boolean incorrect = true;
         Random randomNumber = new Random();
-        int magicNumber = randomNumber.nextInt(20) + 1;
-        System.out.println("Hello! What is your name?");
-        String name = scan.next();
-        System.out.println("Well, " + name + ", I am thinking of a number between 1 and 20.");
-        do {
+        Scanner scan = new Scanner(System.in);
+        intro(scan);
+        while(incorrect) {
+            magicNumber = randomNumber.nextInt(20) + 1;
+            int count = 0;
             do {
                 count++;
-                System.out.println("Take a guess.");
-                guess = scan.nextInt();
+                startGuessing();
+                while(true) {
+                    try {
+                        guess = Integer.parseInt(scan.next());
+                        break;
+                    } catch(Exception e) {
+                        System.out.println("You must guess an integer between 1 and 20.");
+                        startGuessing();
+                    }
+                }
                 if (guess > magicNumber) {
                     System.out.println("Your guess is too high.");
                 } else if (guess < magicNumber) {
@@ -25,14 +34,24 @@ public class GuessTheNumber {
                     System.out.println("Good job, " + name + "! You guessed my number in " + count + " guesses!");
                 }
             } while (magicNumber != guess);
-            System.out.println("Would you like to play again? (y or n)");
-            String playAgain = scan.next();
-            if (playAgain.equals("y")) {
-                yes = true;
-            } else {
-                yes = false;
+
+            System.out.println("Press 'y' if you would like to play again.");
+            playAgain = scan.next().toLowerCase();
+
+            if (!playAgain.equals("y")) {
                 System.out.println("Thanks for playing!");
+                incorrect = false;
             }
-        } while(yes);
+        }
     }
+    public static void intro(Scanner scanner) {
+        System.out.println("Hello! What is your name?");
+        String name = scanner.next();
+        System.out.println("Well, " + name + ", I am thinking of a number between 1 and 20.");
+    }
+
+    public static void startGuessing() {
+        System.out.println("Take a guess.");
+    }
+
 }
